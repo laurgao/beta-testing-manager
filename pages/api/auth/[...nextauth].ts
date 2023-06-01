501d4bfd6e19d6a49bc234be1f959473bdc08f42
Laura Gao <gaolauro@gmail.com>
2023-06-01 02:10:59 -0700

    delete new version nextauth code

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,31 +1,3 @@
-// import NextAuth from "next-auth";
-// // import GoogleProvider from "next-auth/providers/google";
-
-// export const authOptions = {
-//     providers: [
-//         GoogleProvider({
-//             clientId: process.env.GOOGLE_CLIENT_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-//         }),
-//     ]
-// };
-
-// // export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
-
-
-
-// // export const authOptions = {
-// //   // Configure one or more authentication providers
-// //   providers: [
-// //     GithubProvider({
-// //       clientId: process.env.GITHUB_ID,
-// //       clientSecret: process.env.GITHUB_SECRET,
-// //     }),
-// //     // ...add more providers here
-// //   ],
-// // }
-// export default NextAuth(authOptions)
-
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
